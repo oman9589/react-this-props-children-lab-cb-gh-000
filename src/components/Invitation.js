0e4848ed5970dea62a950251d25ebda50a7131e0
Oliver Spring <oliver9589@gmail.com>
2017-09-17 19:16:35 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,21 @@
-// Code Invitation Component Here
+import React from 'react';
+
+class Invitation extends React.Component {
+  render() {
+
+    const children = React.Children.map(this.props.children, child => {
+      return (
+        <div> {child} </div>
+      );
+    });
+
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {children}
+      </div>
+    )
+  }
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedDecoraction extends React.Component {
+  render() {
+    const children = React.Children.map(this.props.children, child => {
+      return (
+        <div className={this.props.theme}> {child} </div>
+      );
+    });
+    return (
+      <div>
+        {children}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecoraction;
